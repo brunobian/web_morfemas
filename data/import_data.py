1452ea6f2e559b58f1a7e1a0d41d8acda4b64c2e
pindonga <brunobian@gmail.com>
2020-07-22 08:55:11 -0300

    se viene la DB

--- data/import_data.py
+++ data/import_data.py
@@ -2,19 +2,17 @@
 
 import psycopg2
 import io
-f = io.open('textos.csv', encoding='latin-1')
 
-#f = open('textos.csv','r')
-
-database='oraciones_2020_new'
+database='morfemas'
 host='localhost'
-user='cloze'
-password='23571113'
+user='brunobian'
+password='tusam.vive'
 connectStr="dbname='"+database+"' user='"+user+"' host='"+host+"' password='"+password+"'"
 
 conn = psycopg2.connect(connectStr)
 cur = conn.cursor()
 
+f = io.open('textos.csv', encoding='latin-1')
 db_text = []
 for l in f.readlines():
     sl = l.split(',',3)
@@ -33,30 +31,3 @@ cur.executemany('INSERT INTO cloze_text  VALUES (%s,%s,%s,%s)',db_text)
 conn.commit()
 f.close()
 cur = conn.cursor()
-
-f = open('exp.csv','r')
-db_exp = []
-for l in f.readlines():
-    sl = l.split(',',3)
-    id = int(sl[0])
-    text_id = int(sl[1])
-    missing = sl[2].replace(';',',')[0:-1]
-    print (id, text_id, missing)
-    
-    db_exp.append((id,text_id,missing))
-    
-cur.executemany('INSERT INTO cloze_trialoption (id, text_id, missing_words) VALUES (%s,%s,%s)',db_exp)
-conn.commit()
-f.close()
-cur = conn.cursor()
-
-f = open('72listasdeexps.csv','r')
-db_seq = []
-for l in f.readlines():
-    
-    db_seq.append(('[' + l.strip() + ']',))
-
-print(db_seq)
-cur.executemany('INSERT INTO cloze_trialsequence (seq) VALUES (%s)',db_seq)
-conn.commit()
-
