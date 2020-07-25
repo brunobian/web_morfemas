ec8a6b03991d733ca0accfa7e085d11a5eaa43ca
pindonga <brunobian@gmail.com>
2020-07-25 20:03:42 -0300

    ordeno las columnas del df

--- morfemas/views.py
+++ morfemas/views.py
@@ -41,11 +41,13 @@ def search(request):
 		por_palabra = True
 		search = pd.DataFrame(list(Palabra.objects.filter(palabra=q[k]).values()))
 		search = search[['palabra', 'sufijo', 'numero', 'sufijada']]
+		search.columns = ['Palabra', 'Sufijo', 'Número', 'Sufijada']
 		sufijo = search['sufijo'][0]
 	elif k == 'palsPorSuj':
 		por_palabra = True
 		search = pd.DataFrame(list(Palabra.objects.filter(sufijo=q[k]).values()))		
 		search = search[['palabra', 'sufijo', 'numero', 'sufijada']]
+		search.columns = ['Palabra', 'Sufijo', 'Número', 'Sufijada']
 		sufijo = search['sufijo'][0]
 		
 	s_html = search.to_html(index=False)
