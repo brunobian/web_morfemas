77db197fe5136750c6e5f4e38e22056378f28114
pindonga <brunobian@gmail.com>
2020-07-26 08:05:59 -0300

    acomodo botones

--- morfemas/views.py
+++ morfemas/views.py
@@ -32,34 +32,32 @@ def search(request):
 	k = keys[0]
 	
 	warning = False
-	palabra = ''
 	if k == 'sufijo':
 		search = pd.DataFrame(list(Sufijo.objects.filter(sufijo=q[k]).values()))
 		search = search[['sufijo', 'numero', 'count_afijada', 'frec_afijada', 'count_pseudoafijada', 'frec_pseudoafijada', 'prop_count_afij', 'prop_frec_afij']]
 		search.columns = ['Sufijo', 'Número', 'Count afijadas', 'Freq afijadas', 'Count pseudoafijadas', 'Freq pseudoafijadas', 'Prop Count Afijadas', 'Prop Freq Afijadas']
-		sufijo = q[k]
-		if sufijo == 'aca':
+		newSearch = q[k]
+		if newSearch == 'aca':
 			warning = True
 	elif k == 'palabra':
-		palabra = q[k]
 		search = pd.DataFrame(list(Palabra.objects.filter(palabra=palabra).values()))
 		search = search[['palabra', 'numero', 'sufijo', 'sufijada']]
 		search.columns = ['Palabra', 'Número', 'Sufijo', 'Sufijada']
-		sufijo = search['Sufijo'][0]
+		newSearch = search['Sufijo'][0]
 	elif k == 'palsPorSuj':
-		palabra = q[k]
 		search = pd.DataFrame(list(Palabra.objects.filter(sufijo=q[k]).values()))		
 		search = search[['palabra', 'numero', 'sufijo', 'sufijada']]
 		search.columns = ['Palabra', 'Número', 'Sufijo', 'Sufijada']
-		sufijo = search['Sufijo'][0]
+		newSearch = search['Sufijo'][0]
 	
 	s_html = search.to_html(index=False)
+	bajar = q[k]
 	
 	c={'request':request,
 		'search':s_html,
 		'search_type':k,
-		'sufijo':sufijo,
-		'palabra':palabra,
+		'newSearch':newSearch,
+		'bajar':bajar,
 		'warning':warning}
 
 	if q['bajar'] == 'True':
