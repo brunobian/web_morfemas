50ab398dcf51fd8f54ea498d4e9728f1e8728b77
pindonga <brunobian@gmail.com>
2020-10-17 12:07:38 -0300

    arreglo el error de las busquedas vacias

--- morfemas/views.py
+++ morfemas/views.py
@@ -34,11 +34,12 @@ def search(request):
 	warning = False
 	if k == 'sufijo':
 		search = pd.DataFrame(list(Sufijo.objects.filter(sufijo=q[k].lower()).values()))
-		search = search[['sufijo', 'numero', 'count_afijada', 'frec_afijada', 'count_pseudoafijada', 'frec_pseudoafijada', 'prop_count_afij', 'prop_frec_afij']]
-		search.columns = ['Sufijo', 'Número', 'Count afijadas', 'Freq afijadas', 'Count pseudoafijadas', 'Freq pseudoafijadas', 'Prop Count Afijadas', 'Prop Freq Afijadas']
-		newSearch = q[k]
-		if newSearch == 'aca':
-			warning = True
+		try:
+			search = search[['sufijo', 'numero', 'count_afijada', 'frec_afijada', 'count_pseudoafijada', 'frec_pseudoafijada', 'prop_count_afij', 'prop_frec_afij']]
+			search.columns = ['Sufijo', 'Número', 'Count afijadas', 'Freq afijadas', 'Count pseudoafijadas', 'Freq pseudoafijadas', 'Prop Count Afijadas', 'Prop Freq Afijadas']
+			newSearch = q[k]
+			if newSearch == 'aca':
+				warning = True					
 	elif k == 'palabra':
 		search = pd.DataFrame(list(Palabra.objects.filter(palabra=q[k].lower()).values()))
 		search = search[['palabra', 'numero', 'sufijo', 'sufijada']]
