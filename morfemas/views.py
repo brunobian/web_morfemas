242823fd3c66f6af2ab8bd983e32f981189d63d4
pindonga <brunobian@gmail.com>
2020-11-09 10:41:51 -0300

    arranco a probar si carga bien plurales y alomorfos

--- morfemas/views.py
+++ morfemas/views.py
@@ -41,7 +41,7 @@ def search(request):
 		tmp = pd.concat([sing, plur])
 		
 		search = pd.DataFrame(list(Sufijo.objects.filter(sufijo=tmp['singular'][0]).values()))		
-		search = pd.DataFrame(list(Sufijo.objects.filter(sufijo=q[k].lower()).values()))		
+		# ~ search = pd.DataFrame(list(Sufijo.objects.filter(sufijo=q[k].lower()).values()))		
 
 		try:
 			search = search[['sufijo', 'numero', 'frec_afijada', 'frec_pseudoafijada', 'prop_frec_afij', 'count_afijada', 'count_pseudoafijada',  'prop_count_afij', ]]
@@ -73,7 +73,7 @@ def search(request):
 	# ~ search.drop_duplicates(inplace=True)
 	empty  = search.empty			
 	s_html = search.to_html(index=False)
-	s_html = tmp.to_html(index=False)
+	# ~ s_html = tmp.to_html(index=False)
 
 	bajar  = q[k]
 	
