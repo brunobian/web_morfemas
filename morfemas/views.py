46f28638087195008356242204458787b7376347
pindonga <brunobian@gmail.com>
2020-11-21 10:08:40 -0300

    vuelvo a eliminar duplicado

--- morfemas/views.py
+++ morfemas/views.py
@@ -81,8 +81,9 @@ def search(request):
 	
 	
 	empty  = search.empty			
-	s_html = search.to_html(index=False)
 	search.drop_duplicates(inplace=True)
+	s_html = search.to_html(index=False)
+
 	# ~ tmp.drop_duplicates(inplace=True)
 	# ~ s_html = tmp.to_html(index=False)
 
